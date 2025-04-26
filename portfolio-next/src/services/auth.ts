103e6d4af348ab95c3cf592ec3c855e300be6674
Mirza <redzacs@gmail.com>
2025-04-26 23:48:52 +0200

    dasdada

--- portfolio-next/src/services/auth.ts
+++ portfolio-next/src/services/auth.ts
@@ -1,27 +1,15 @@
+// src/services/auth.ts
+
+import axiosInstance from '@/api/axiosInstance';
+
 export const authService = {
-    async login(email: string, password: string) {
-        const response = await fetch('http://portfolio:8443/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            body: JSON.stringify({ email, password }),
-        });
+  async login(email: string, password: string) {
+    const { data } = await axiosInstance.post('/login', { email, password });
+    return data.data;
+  },
 
-        if (!response.ok) {
-            throw new Error('Login failed');
-        }
-
-        return response.json();
-    },
-
-    async logout() {
-        const response = await fetch('http://portfolio:8443/api/logout', {
-            method: 'GET',
-            credentials: 'include',
-        });
-
-        return response.json();
-    }
+  async logout() {
+    const { data } = await axiosInstance.get('/logout');
+    return data;
+  },
 };
