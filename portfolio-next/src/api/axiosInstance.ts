a9633b449835b9ecc857207edf40620d06ea633f
Mirza <redzacs@gmail.com>
2025-04-27 15:39:11 +0200

    dsadas

--- portfolio-next/src/api/axiosInstance.ts
+++ portfolio-next/src/api/axiosInstance.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://mirzaredzic.hopto.org/api',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://mirzaredzic.hopto.org',
   headers: {
     'Accept': 'application/json',
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
