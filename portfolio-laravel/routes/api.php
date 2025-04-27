c5bf965bb80a21ddebba9f53fdec0d894cfefd64
Mirza <redzacs@gmail.com>
2025-04-27 15:35:16 +0200

    dsadsa

--- portfolio-laravel/routes/api.php
+++ portfolio-laravel/routes/api.php
@@ -6,23 +6,22 @@ use App\Http\Controllers\Auth\LoginController;
 
 use App\Http\Controllers\Auth\ContentController;
 
+Route::prefix('api')->group(function () {
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/logout', [LoginController::class, 'logout']);
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
 
-Route::post('/login', [LoginController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/logout', [LoginController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+        Route::prefix('contents')->group(function () {
+            Route::get('', [ContentController::class, 'getContent']);
+            Route::post('', [ContentController::class, 'storeContent']);
+            Route::get('/{content}', [ContentController::class, 'showContent']);
+            Route::put('/{content}', [ContentController::class, 'updateContent']);
+            Route::delete('/{content}', [ContentController::class, 'destroyContent']);
+        });
     });
-
-    Route::prefix('contents')->group(function () {
-        Route::get('', [ContentController::class, 'getContent']);
-        Route::post('', [ContentController::class, 'storeContent']);
-        Route::get('/{content}', [ContentController::class, 'showContent']);
-        Route::put('/{content}', [ContentController::class, 'updateContent']);
-        Route::delete('/{content}', [ContentController::class, 'destroyContent']);
-    });
-
-    
 });
 
 
