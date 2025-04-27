85be153608ffc737973169ab34aa390bd4356dcc
Mirza <redzacs@gmail.com>
2025-04-27 20:51:06 +0200

    dsada

--- portfolio-next/src/app/api.ts
+++ portfolio-next/src/app/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://portfolio:8443/api',
+    baseURL: 'https://mirzaredzic.hopto.org/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
