0094bf40eee56aa03684392254edb5ad446fcea6
Mirza <redzacs@gmail.com>
2025-05-18 15:54:11 +0200

    dsada

--- portfolio-next/src/lib/axios.ts
+++ portfolio-next/src/lib/axios.ts
@@ -18,7 +18,8 @@ const api = axios.create({
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
     }
 });
 
